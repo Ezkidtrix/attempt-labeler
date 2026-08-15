c5f58109bca4adf3c0c28f72674f5628a3742c96
BuildTools <unconfigured@null.spigotmc.org>
2026-08-15 09:05:34 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,92 @@
+#include "Geode/cocos/label_nodes/CCLabelBMFont.h"
+#include <Geode/Geode.hpp>
+#include <Geode/loader/SettingV3.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+struct Settings {
+  bool enabled = true;
+  int triggerChance = 500;
+};
+static Settings settings;
+
+std::vector<std::string> phrases;
+
+void getPhrases() {
+  std::vector<std::string> phrasesSetting = Mod::get()->getSavedValue<std::vector<std::string>>("phrases");
+  std::vector<std::string> defaultPhrases = {
+    "You got this!",
+    "Keep going!",
+    "One more attempt.",
+    "Stay focused.",
+    "That was close!",
+    "We are so back.",
+    "Don't choke now.",
+    "Skill issue detected.",
+    "The level has spoken.",
+    "Have you considered winning?",
+    "You're cooking!",
+    "Lock in!",
+    "That never happened.",
+    "Blame the hitbox.",
+    "Absolutely intentional.",
+    "We don't talk about that.",
+    "Massive W incoming.",
+    "The cube has betrayed you.",
+    "Perhaps try not dying.",
+    "New strategy: survive."
+  };
+
+  if (phrasesSetting.empty()) {
+    phrases = defaultPhrases;
+    Mod::get()->setSavedValue("phrases", defaultPhrases);
+  } else {
+    phrases = phrasesSetting;
+  }
+}
+
+std::string randomLabel() {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+
+  std::uniform_int_distribution<> dis(0, phrases.size() - 1);
+  return phrases[dis(gen)];
+}
+
+class $modify(MyPlayLayer, PlayLayer) {
+  bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+    if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+    if (!settings.enabled) return true;
+
+    getPhrases();
+    return true;
+  }
+
+  void destroyPlayer(PlayerObject* player, GameObject* object) {
+    PlayLayer::destroyPlayer(player, object);
+    
+    if (!settings.enabled) return;
+    std::string text = randomLabel();
+
+    auto label = CCLabelBMFont::create(text.c_str(), "bigFont.fnt");
+    label->setScale(0.6);
+
+    float height = object->getContentHeight() * object->m_height;
+    float offset = (height / 2.0f) + 15.0f;
+
+    CCPoint pos = object->getPosition();
+    label->setPosition(CCPoint{ pos.x, pos.y + offset });
+
+    m_objectLayer->addChild(label, 1000);
+  }
+};
+
+$on_mod(Loaded) {
+  getPhrases();
+  settings.enabled = Mod::get()->getSettingValue<bool>("enabled");
+
+  listenForSettingChanges<bool>("enabled", [](bool value) {
+    settings.enabled = value;
+  });
+};
